8ed2482680491b4d82aa7d2996139da46bf243f4
Remmelt Ammerlaan <reammerl@microsoft.com>
2022-02-10 14:03:53 +0100

    bug fixes

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
 
 namespace StarChart.Models
 {

0b3a892a643efb5cd0381105d13e61ee7d6f10a6
Remmelt Ammerlaan <reammerl@microsoft.com>
2022-02-10 14:15:34 +0100

    add Controller class

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
